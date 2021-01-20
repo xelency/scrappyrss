b56b78cb82782d474180a57812b12bf0bf4cf1dc
Xelency <game.server@windowslive.com>
2021-01-20 23:09:14 +0200

    items list

--- scrappyrss.py
+++ scrappyrss.py
@@ -2,15 +2,29 @@
 
 import requests
 from bs4 import BeautifulSoup
+import lxml
 
 # scraping function
 def run_rss(target_rss):
-    try:
-        r = requests.get(target_rss)
-        return print('The scraping job succeeded: ', r.status_code)
-    except Exception as e:
-        print('The scraping job failed. See exception: ')
-        print(e)
+	article_list = []
+	try:
+		r = requests.get(target_rss)
+		soup = BeautifulSoup(r.content, features='xml')
+		articles = soup.findAll('item')
+		for a in articles:
+			title = a.find('title').text
+			link = a.find('link').text
+			pubdate = a.find('pubDate').text
+			article = {
+			'title': title,
+			'link': link,
+			'published': pubdate
+			}
+			article_list.append(article)
+		return print(article_list)
+	except Exception as e:
+		print('The scraping job failed. See exception: ')
+		print(e)
 print('Starting scraping')
 run_rss('https://www.bloomberg.com/opinion/authors/ARbTQlRLRjE/matthew-s-levine.rss')
 print('Finished scraping')
